6ce36d3925a88bd8ac2659697775f994483d95d5
justinfurstoss-branch <justin.furstoss@branch.io>
2020-03-18 08:51:10 -0400

    Initial commit

---
+++ AasthaDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AasthaDemoApp
+//
+//  Created by Justin Furstoss on 3/18/20.
+//  Copyright © 2020 Justin Furstoss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
